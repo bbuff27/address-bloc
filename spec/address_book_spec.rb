104a4ab3b5f29c539dbaef4f0295c853d9969130
Bill Buff <bbuff27@gmail.com>
2018-03-21 19:17:22 -0600

    Create entries2 tests

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -99,4 +99,34 @@ RSpec.describe AddressBook do
 			check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
 		end
 	end
+
+	describe "imports from entries2.csv file" do
+		it "imports the correct number of entries" do
+			book.import_from_csv("entries2.csv")
+			book_size = book.entries.size
+
+			expect(book_size).to eq(3)
+		end
+
+		it "imports the 1st entry" do
+			book.import_from_csv("entries2.csv")
+			entry_one = book.entries[0]
+
+			check_entry(entry_one, "JimBob", "123-456-7890", "jimbob@hotmail.com")
+		end
+
+		it "imports the 2nd entry" do
+			book.import_from_csv("entries2.csv")
+			entry_two = book.entries[1]
+
+			check_entry(entry_two, "Roxanne", "123-867-5309", "roxy@yahoo.com")
+		end
+
+		it "imports the 3rd entry" do
+			book.import_from_csv("entries2.csv")
+			entry_three = book.entries[2]
+			
+			check_entry(entry_three, "Zoe", "123-365-8687", "zoester@gmail.com")
+		end
+	end
 end
